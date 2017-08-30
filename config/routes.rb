4baef431cdc2527c34a61ed81a2c1625dfe48a65
Magda Mieczynska <magda.mieczynska@gmail.com>
2017-08-30 12:10:02 +0200

    Update routes with product types

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :storages do
     resources :products
   end
+  resources :product_types
+  get 'search/:storage_id' => 'search#index', as: 'search'
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
